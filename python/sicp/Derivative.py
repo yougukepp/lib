8fd4f35dce8e7d27f1fff056826cb1d0ee32268b
PengPeng <yougukepp@gmail.com>
2013-05-25 15:45:54 +0800

    修复Derivative.py的Bug

--- python/sicp/Derivative.py
+++ python/sicp/Derivative.py
@@ -9,7 +9,7 @@ from tool.base import Square
 def Derivative(func):
   dx = 1e-6
   if not callable(func):
-    print(str(deal), "不可调用.")
+    print(str(func), "不可调用.")
     return None
 
   rst = lambda x : (func(x + dx) - func(x)) / dx
