70a0ad29585ee11da62bbafae9e651d675fa2f85
PengPeng <yougukepp@gmail.com>
2013-07-25 11:49:44 +0800

    GA加入菜单栏和工具链

--- python/GA/gui/HyGaMainWindow.py
+++ python/GA/gui/HyGaMainWindow.py
@@ -9,29 +9,63 @@ from gui.HyGaCtrlWidget import HyGaCtrlWidget
 from gui.HyGaCanvas import HyGaCanvas
 
 class HyGaMainWindow(QMainWindow):
-  def __init__(self, parent=None):
-    QMainWindow.__init__(self, parent)
-    self.setWindowTitle("计算几何 测试")
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent)
+        self.setWindowTitle("计算几何 测试")
 
-    # 中心画布
-    self.mCanvas = HyGaCanvas()
-    self.setCentralWidget(self.mCanvas)
+        """
+        菜单栏
+        """
+        newAct = QAction("&新建", self)
+        newAct.setShortcuts(QKeySequence.New)
+        newAct.setStatusTip("新建一个工程")
+        newAct.triggered.connect(self.newProject)
 
-    # 状态栏
-    self.mStatusBar = HyGaStatusBar()
-    self.setStatusBar(self.mStatusBar)
+        openAct = QAction("&打开", self)
+        openAct.setShortcuts(QKeySequence.Open)
+        openAct.setStatusTip("打开存在的工程")
+        openAct.triggered.connect(self.openProject)
 
-    # 控制(dock)框
-    self.mCtrlWidget = HyGaCtrlWidget()
-    ctrlDock = QDockWidget("控制")
-    self.addDockWidget(Qt.RightDockWidgetArea, ctrlDock)
-    ctrlDock.setWidget(self.mCtrlWidget)
-    ctrlDock.setFeatures(QDockWidget.NoDockWidgetFeatures)
+        fileMenu = self.menuBar().addMenu("&工程")
+        fileMenu.addAction(newAct)
+        fileMenu.addAction(openAct)
 
-    # 绑定信号和槽
-    self.mCtrlWidget.msCurrentChanged.connect(self.mStatusBar.CurrentChanged)
-    self.mCanvas.msMove.connect(self.mStatusBar.Move)
-    self.mCtrlWidget.mButtonRun.clicked.connect(self.mCanvas.DrawConvexHull)
+        """
+        工具栏
+        """
+        fileToolBar = self.addToolBar("工程")
+        fileToolBar.addAction(newAct)
+        fileToolBar.addAction(openAct)
+
+        fileToolBar.setAllowedAreas(Qt.TopToolBarArea | Qt.BottomToolBarArea)
+        self.addToolBar(Qt.TopToolBarArea, fileToolBar)
+
+        # 中心画布
+        self.mCanvas = HyGaCanvas()
+        self.setCentralWidget(self.mCanvas)
+
+        # 状态栏
+        self.mStatusBar = HyGaStatusBar()
+        self.setStatusBar(self.mStatusBar)
+
+        # 控制(dock)框
+        self.mCtrlWidget = HyGaCtrlWidget()
+        ctrlDock = QDockWidget("控制")
+        self.addDockWidget(Qt.RightDockWidgetArea, ctrlDock)
+        ctrlDock.setWidget(self.mCtrlWidget)
+        ctrlDock.setFeatures(QDockWidget.NoDockWidgetFeatures)
+
+        # 绑定信号和槽
+        self.mCtrlWidget.msCurrentChanged.connect(self.mStatusBar.CurrentChanged)
+        self.mCanvas.msMove.connect(self.mStatusBar.Move)
+        self.mCtrlWidget.mButtonRun.clicked.connect(self.mCanvas.DrawConvexHull)
+
+    def newProject(self):
+        pass
+
+    def openProject(self):
+        pass
+
 
 if __name__ == "__main__":
     import sys
