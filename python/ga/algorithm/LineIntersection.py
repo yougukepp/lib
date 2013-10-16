7bc37f4ee39d1ede1d7e54b2859960d5bed5fc61
PengPeng <yougukepp@gmail.com>
2013-10-16 08:52:56 +0800

    增加事件队列的 类桩

--- python/ga/algorithm/LineIntersection.py
+++ python/ga/algorithm/LineIntersection.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from dataType.EventQueue import EventQueue
+
 def LineIntersection(lineList, IntersectionDict): 
     print("实现中......")
     print("输入线集,如下:")
     for line in lineList:
         line.Print()
         print()
+
+    # 新建事件队列
+    eventQueue = EventQueue()
 
 if __name__ == "__main__":
     print("测试LineIntersection算法:通过")
---
+++ python/ga/dataType/EventQueue.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class EventQueue():
+    def __init__(self):
+        pass
+
+    def GetHighestEvent(self):
+        pass
+
+    def InsertEvent(self):
+        pass
+
+if __name__ == "__main__":
+    print("测试EventQueue算法:通过")
+
