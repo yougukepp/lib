d5fd9a6019cba08621482a010fb78f4e7336475e
PengPeng <pengpeng@fiberhome.com>
2015-04-02 15:15:59 +0800

    修复nand测试脚本，使其与设备更匹配

--- c/dev_test/nand_test/run.sh
+++ c/dev_test/nand_test/run.sh
@@ -2,5 +2,5 @@
 
 #命令名字   内存大小(kB) 文件大小(MB) 测试次数 打印间隔(秒) 文件目录
 
-./nand_test 2048         130          1000     2            /fhcfg/nand.test
+./nand_test 1024 3 99999 2 /fhcfg/nand_test/nand.test
 
