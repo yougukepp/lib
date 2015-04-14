592c3c1248d4a0cba1cd00e521cc50bb2f4a51da
PengPeng <pengpeng@fiberhome.com>
2015-04-14 18:51:55 +0800

    完善数据解析

--- python/bigdata/net.py
+++ python/bigdata/net.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import json
 import urllib.request
 
 g_gdp_url = 'http://api.worldbank.org/zh/countries/all/indicators/NY.GDP.MKTP.CD?format=json&per_page=10&page=1'
@@ -9,6 +10,17 @@ g_population_url = 'http://api.worldbank.org/zh/countries/all/indicators/SP.POP.
 if __name__ == '__main__':
     response = urllib.request.urlopen(g_gdp_url)
     buff = response.read()
-    json = buff.decode("utf8")
-    print(json)
+    buff_utf8 = buff.decode("utf8")
+    data = json.loads(buff_utf8)
+
+    print(data)
+
+    head = data[0]
+    content = data[1]
+    total = head['total']
+
+    print(total)
+
+    for v in content:
+        print(v)
 
