0fa1278390e04d8a5621495ccda5d8c6460eef98
PengPeng <pengpeng@fiberhome.com>
2015-04-15 13:52:51 +0800

    完善数据解析

--- python/bigdata/net.py
+++ python/bigdata/net.py
@@ -4,41 +4,114 @@
 import json
 import urllib.request
 
-g_gdp_url = 'http://api.worldbank.org/zh/countries/all/indicators/NY.GDP.MKTP.CD?format=json&per_page=10&page=1'
-g_population_url = 'http://api.worldbank.org/zh/countries/all/indicators/SP.POP.TOTL?format=json&per_page=10&page=1'
+g_base_url = 'http://api.worldbank.org/zh/countries/all/indicators/'
 
-if __name__ == '__main__':
-    head_index = 0
-    content_index = 1
-    total_key = 'total'
-    date_key = 'date'
-    country_key = 'country'
-    country_v_key = 'value'
-    value_key = 'value'
+g_tyep_dict = {
+        'gdp':'NY.GDP.MKTP.CD',
+        'population':'SP.POP.TOTL'
+}
 
-    response = urllib.request.urlopen(g_gdp_url)
+g_head_index = 0
+g_content_index = 1
+g_total_key = 'total'
+g_date_key = 'date'
+g_country_key = 'country'
+g_country_v_key = 'value'
+g_value_key = 'value'
+
+g_once_count = 100
+
+def parse_data(data):
+    """
+    格式
+    {(year,国):值}}
+    """
+    parsed_data = {}
+
+    content_all = data[g_content_index]
+    for content in content_all:
+        date = content[g_date_key]
+        country = content[g_country_key][g_country_v_key]
+        value = content[g_value_key]
+
+        parsed_data[(date,country)] = value
+
+    return parsed_data
+
+def parse_count(data):
+    head = data[g_head_index]
+    total = head[g_total_key]
+
+    return total
+
+def get_net_data(url):
+    response = urllib.request.urlopen(url)
     buff = response.read()
     buff_utf8 = buff.decode("utf8")
     data = json.loads(buff_utf8)
 
-    # 生成头 和 内容
-    head = data[head_index]
-    content_all = data[content_index]
-    # 获取所有内容(content)数目
-    total = head[total_key]
+    return data
 
-    #print(data)
+def mkdir_url(value_type, per_page, page):
+    url = g_base_url + g_tyep_dict[value_type]
+    url += '?format=json&per_page=' + str(per_page)
+    url += '&page=' + str(page)
 
-    get_count = 0
-    print("共有 %d 个数据项目:" % total)
-    for content in content_all:
-        date = content[date_key]
-        country = content[country_key][country_v_key]
-        value = content[value_key]
+    #print(url)
 
-        print(date, country, value)
+    return url
 
-        get_count += 1
+def get_count(value_type):
+    url = mkdir_url(value_type, 1, 1)
+    data = get_net_data(url)
+    count = parse_count(data)
 
-    print("获取 %d:" % get_count)
+    return count;
+
+def get_data(value_type, per_page, page):
+    url = mkdir_url(value_type, per_page, page)
+    data = get_net_data(url)
+    parsed_data = parse_data(data)
+
+    return parsed_data
+
+def get_all_data(value_type):
+    total = get_count(value_type)
+    page_max = int(total / g_once_count) + 1
+
+    all_data = {}
+
+    for i in range(1, page_max): 
+        data = get_data(value_type, g_once_count, i)
+        rate = 100.0 * i * g_once_count / total
+        print("%.2f%%" % rate)
+
+        # 合并入新数据
+        all_data.update(data)
+
+    return all_data
+
+if __name__ == '__main__':
+    gdp_data = get_all_data ('gdp')
+    population_data = get_all_data('population')
+
+    """
+    格式
+    {(year,国):(值1, 值2,......}}
+    """
+    all_data = {}
+    for k in gdp_data:
+        all_data[k] = (gdp_data[k], population_data[k])
+
+
+
+    # 调试信息
+
+    for k in all_data:
+        print(k[0], k[1], all_data[k])
+
+    out = open('data.txt', 'w')
+    json_str = json.dumps(all_data, indent=4)
+    out.write(json_str)
+    out.close()
 
