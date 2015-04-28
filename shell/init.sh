c59b920a853fbdeecb0c4af68eb8d3c2db08c38c
PengPeng <pengpeng@fiberhome.com>
2015-04-28 20:57:18 +0800

    modify init.sh for jessie

--- shell/init.sh
+++ shell/init.sh
@@ -3,15 +3,7 @@
 # Base
 sudo apt-get install git sudo vim build-essential autoconf automake gcc binutils libc6 linux-headers-$(uname -r) libtool
 
-sudo apt-get install libc6-dev libncurses5-dev libreadline6-dev g++-multilib cscope ctags cflow ttf-wqy-microhei ttf-wqy-zenhei ntpdate sudo vim vim-tiny mpg123 gdb gnuplot flashplugin-nonfree tree rar unrar zip meld mplayer2 gnome-mplayer xfonts-intl-chinese xfonts-wqy gedit p7zip p7zip-full p7zip-rar pkg-config flex bison sed gawk bc u-boot-tools minicom sysv-rc-conf sylpheed 
-sudo apt-get install x-window-system-core 
-sudo apt-get install gnome-core
-
-
-# firefox 可以看chm文件
-#chmsee 
-# TODO: fix this problem
-#sudo apt-get ia32-libs
+sudo apt-get install libc6-dev libncurses5-dev libreadline6-dev g++-multilib cscope ctags cflow ttf-wqy-microhei ttf-wqy-zenhei ntpdate sudo vim vim-tiny mpg123 gdb gnuplot flashplugin-nonfree tree rar unrar zip meld mplayer2 gnome-mplayer xfonts-intl-chinese xfonts-wqy gedit p7zip p7zip-full p7zip-rar pkg-config flex bison sed gawk bc u-boot-tools minicom sysv-rc-conf sylpheed zlib1g-dev subversion x-window-system-core gnome-core
 
 # 博客
 #sudo apt-get install jekyll python-pygments ruby-rdiscount 
@@ -22,8 +14,8 @@ sudo apt-get install gnome-core
 # octave 
 #sudo apt-get install octave
 
-#broken
-#sudo apt-get install scim scim-tables-zh
+# input method
+sudo apt-get install fcitx-googlepinyin
 
 # document
 sudo apt-get install binutils-doc cpp-doc gcc-doc glibc-doc stl-manual manpages-zh manpages-dev manpages-posix-dev manpages-posix libx11-doc man2html
@@ -42,33 +34,34 @@ sudo apt-get install binutils-doc cpp-doc gcc-doc glibc-doc stl-manual manpages-
 sudo apt-get install python3-pyqt5
 
 # python & serial
-sudo apt-get install python-serial
+sudo apt-get install python3-serial
 
 # OpenGL
-#sudo apt-get install libegl1-mesa-dev libgles2-mesa-dev libopenvg1-mesa-dev libgles2-mesa-dev freeglut3-dev python-opengl libglew-dev libglui-dev
+sudo apt-get install libegl1-mesa-dev libgles2-mesa-dev libopenvg1-mesa-dev libgles2-mesa-dev freeglut3-dev python-opengl libglew-dev libglui-dev
 
 # Game
 #sudo apt-get install wesnoth
 
 # Tex
-#sudo apt-get install asymptote texlive-full asymptote-doc texlive-doc-zh
-#sudo apt-get install doxygen
+sudo apt-get install asymptote texlive-full asymptote-doc texlive-doc-zh
+sudo apt-get install doxygen
 
 # gnuradio
 #sudo apt-get install gnuradio
 
-# ssh & samba & nfs
+# ssh & samba & nfs & tftp
 # sudo apt-get install openssh-server ssh samba nfs-common nfs-kernel-server portmap
+sudo  apt-get install tftp tftpd xinetd
 
 # update
-#sudo apt-get update
-#sudo apt-get upgrade
+sudo apt-get update
+sudo apt-get upgrade
 
-#sudo apt-get autoremove
-#sudo apt-get autoclean
+sudo apt-get autoremove
+sudo apt-get autoclean
 
 # ntp time 
 # sudo ntpdate 210.72.145.44      # 国家授时中心
-#sudo ntpdate 202.120.2.101      # 上海交大
-#sudo hwclock -w --localtime
+sudo ntpdate 202.120.2.101      # 上海交大
+sudo hwclock -w --localtime
 
