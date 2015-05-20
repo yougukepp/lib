5a1dd0657fbed9fee62a4a886c7a3d5098a7cf7c
PengPeng <pengpeng@fiberhome.com>
2015-05-20 14:25:27 +0800

    完善串口测试功能

--- python/serial/testSerial.py
+++ python/serial/testSerial.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import serial
+import serial.tools.list_ports
 
 gData = b"hello\x7e\x16"
 
@@ -16,7 +17,14 @@ def test(timeOut, recvBufSize):
     ser.close()
 
 if __name__ == "__main__":
+    print('all serial:')
+    allSerial = serial.tools.list_ports.comports()
+    for s in allSerial:
+        print(s[0])
+    print()
+
     recvBufSize = 2048
     timeOut = 1
     test(timeOut, recvBufSize)
+
 
