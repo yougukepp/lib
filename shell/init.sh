8bec66ce6d08df2a774d2dd875790fd5fb7bf0aa
PengPeng <pengpeng@fiberhome.com>
2015-05-29 14:36:48 +0800

    完善python3 re的demo

--- shell/init.sh
+++ shell/init.sh
@@ -35,10 +35,7 @@ sudo apt-get install binutils-doc cpp-doc gcc-doc glibc-doc stl-manual manpages-
 #sudo apt-get install sbcl sbcl-doc
 
 # python & Qt
-sudo apt-get install python3-pyqt5 libqt5designer5 qt4-designer
-
-# python & serial
-sudo apt-get install python3-serial
+sudo apt-get install python3-pyqt5 libqt5designer5 qt4-designer python3-serial python3-tk
 
 # OpenGL
 sudo apt-get install libegl1-mesa-dev libgles2-mesa-dev libopenvg1-mesa-dev libgles2-mesa-dev freeglut3-dev python-opengl libglew-dev
